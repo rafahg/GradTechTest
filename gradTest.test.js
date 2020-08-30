27fe71aeab9304e043280ed1811a80acacc8c670
Rafael <r.hgar78@gmail.com>
2020-08-30 23:12:10 +0200

    First test passed, green

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,13 +1,22 @@
-
 function createMenuData(data) {
- 
+
+  let splitData = data[0].split("/");
+  let dataSolution = [];
+  let dataList = {};
+  
+  dataList.title = splitData[0];
+  dataList.data = [];
+  dataList.data.push(splitData[1]);
+  dataSolution.push(dataList);
+  
+  return dataSolution;
+
 }
 
 describe("menu Data Generator", () => {
     
   it("works in the simple case scenario, creating correct data structure", () => {
-    const data = [
-      "parent1/parent1child"];
+    const data = ["parent1/parent1child"];
 
     const expectedResult = [
       {
